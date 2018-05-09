4d561b3d38849b4a9143d0ba7257fee18a380aeb
valerieblanchard <vblankwebmaster@gmail.com>
2018-05-09 18:56:16 +0200

    Add missing singular placeholder in comments.php

--- comments.php
+++ comments.php
@@ -28,11 +28,27 @@ if ( post_password_required() ) {
 	?>
 		<h2 class="comments-title">
 			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'flooor' ) ),
-					number_format_i18n( get_comments_number() ),
+			$comments_number = get_comments_number();
+			if ( 1 === $comments_number ) {
+				printf(
+					/* translators: %s: post title */
+					esc_html_x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'flooor' ),
 					'<span>' . get_the_title() . '</span>'
 				);
+			} else {
+				printf( // WPCS: XSS OK.
+					/* translators: 1: number of comments, 2: post title */
+					esc_html( _nx(
+						'%1$s thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'flooor'
+					) ),
+					number_format_i18n( $comments_number ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			}
 			?>
 		</h2><!-- .comments-title -->
 
