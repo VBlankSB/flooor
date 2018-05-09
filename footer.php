b470cd6fccd7fdabec78867163b5d0927274faa4
valerieblanchard <vblankwebmaster@gmail.com>
2018-05-10 01:57:58 +0200

    Fix errors in footer

--- footer.php
+++ footer.php
@@ -16,9 +16,12 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<small>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>
-      <?php printf( esc_html__( 'by %1$s', 'flooor' ), '<a href="https://pixiscreen.fr" rel="designer">VBlank</a>' ); ?>
-      <span class="sep"> | </span>
-      <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'flooor' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'flooor' ), 'WordPress' ); ?></a></small>
+			<?php
+			// translators: 1: Theme author.
+			printf( esc_html__( 'by %1$s', 'flooor' ), '<a href="https://pixiscreen.fr" rel="author">VBlank</a>' );
+			?>
+			<span class="sep"> | </span>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'flooor' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'flooor' ), 'WordPress' ); ?></a></small>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
