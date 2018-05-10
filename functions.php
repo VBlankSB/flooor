aee332e7dacf8b5ddb5ce03e99857634a14d3d8c
valerieblanchard <vblankwebmaster@gmail.com>
2018-05-10 02:13:32 +0200

    Minor correction to functions.php

--- functions.php
+++ functions.php
@@ -7,93 +7,93 @@
  * @package Flooor
  */
 
-/*------------------------------------*\
-	Theme Support & fonctionnality
-\*------------------------------------*/
 if ( ! function_exists( 'flooor_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function flooor_setup() {
-
-	// Make theme available for translation.
-	load_theme_textdomain( 'flooor', get_template_directory() . '/languages' );
-
-	// Add Style of the theme in the TinyMCE visual editor to resemble the theme style.
-	add_editor_style( array( 'assets/css/editor-style.css',  'assets/genericons/genericons.css' ) );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	add_theme_support( 'post-thumbnails' );
+	function flooor_setup() {
+		/**
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on flooor, use a find and replace
+		 * to change 'flooor' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'flooor', get_template_directory() . '/languages' );
 
+		// Add Style of the theme in the TinyMCE visual editor to resemble the theme style.
+		add_editor_style( array( 'assets/css/editor-style.css', 'assets/genericons/genericons.css' ) );
 
-	/*
-	 * This theme uses wp_nav_menu() in 2 locations.
-	 */
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary Menu', 'flooor' ),
-		'social' => esc_html__( 'Social Menu', 'flooor' ),
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-	// Add theme support for post formats
-	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'quote', 'video', 'audio', 'image', 'status', 'chat') );
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'flooor_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+		/*
+		 * This theme uses wp_nav_menu() in 2 locations.
+		 */
+		register_nav_menus( array(
+			'primary' => esc_html__( 'Primary Menu', 'flooor' ),
+			'social'  => esc_html__( 'Social Menu', 'flooor' ),
+		) );
 
-	// Allow upload logo image in the customizer
-	//https://developer.wordpress.org/themes/functionality/custom-logo/
-	add_theme_support( 'custom-logo', array(
-		'height'      => 150,
-		'width'       => 150,
-		'flex-height' => true,
-		'flex-width'  => true,
-		'header-text' => array( 'site-title', 'site-description' ),
-	) );
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
 
-	/*
-	 * Add theme support for selective refresh for widgets.
-	 *
-	 * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#widgets-opting-in-to-selective-refresh
-	 */
-	add_theme_support( 'customize-selective-refresh-widgets' );
+		// Add theme support for post formats.
+		add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'quote', 'video', 'audio', 'image', 'status', 'chat' ) );
 
-	// Add excerpt to pages.
-	add_post_type_support( 'page', 'excerpt' );
-}
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'flooor_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Allow upload logo image in the customizer
+		// https://developer.wordpress.org/themes/functionality/custom-logo/ .
+		add_theme_support( 'custom-logo', array(
+			'height'      => 150,
+			'width'       => 150,
+			'flex-height' => true,
+			'flex-width'  => true,
+			'header-text' => array( 'site-title', 'site-description' ),
+		) );
+
+		/*
+		 * Add theme support for selective refresh for widgets.
+		 *
+		 * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#widgets-opting-in-to-selective-refresh
+		 */
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		// Add excerpt to pages.
+		add_post_type_support( 'page', 'excerpt' );
+	}
 endif;
 add_action( 'after_setup_theme', 'flooor_setup' );
 
@@ -116,7 +116,7 @@ add_action( 'after_setup_theme', 'flooor_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function flooor_widgets_init() {
-	// Define Sidebar Widget Area
+	// Define Sidebar Widget Area.
 	register_sidebar( array(
 		'name'          => esc_html__( 'Main sidebar', 'flooor' ),
 		'id'            => 'main-sidebar',
@@ -134,14 +134,14 @@ add_action( 'widgets_init', 'flooor_widgets_init' );
  * ===========================
  */
 
-define('FLOOOR_VERSION', wp_get_theme()->get( 'Version' ));
+define( 'FLOOOR_VERSION', wp_get_theme()->get( 'Version' ) );
 
 function flooor_scripts() {
-	/* Styles : wp_enqueue_style( $handle, $src, $deps, $ver, $media ) */
-	/*=================================================================*/
-	// Load Theme Stylesheet
-	//wp_enqueue_style( 'flooor-style', get_stylesheet_uri() );
-	// ou
+	/* Styles : wp_enqueue_style( $handle, $src, $deps, $ver, $media )
+		=================================================================*/
+	// Load Theme Stylesheet.
+	// wp_enqueue_style( 'flooor-style', get_stylesheet_uri() );
+	// ou.
 	wp_enqueue_style( 'flooor-style', get_template_directory_uri() . '/style.css', array(), FLOOOR_VERSION, 'all' );
 
 	// Add Genericons font, used in the main stylesheet.
@@ -161,11 +161,12 @@ function flooor_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'flooor_scripts' );
 
-/*----------------------------------------------------*\
-	Fonctions déportées pour faciliter la maintenance
-\*----------------------------------------------------*/
 /**
- * Securize Wordpress
+ *  Fonctions déportées pour faciliter la maintenance
+ *  -------------------------------------------------
+ */
+/**
+ * Securize WordPress
  * Later, it should be in a plugin
  */
 require get_template_directory() . '/inc/securize-wordpress.php';
